d14591acd2cbf686be4003f3cb2f8603ea2d2500
Andrey <42993931+BobrovAndrey@users.noreply.github.com>
2018-09-24 14:45:54 +0300

    "Undefined" information returns. Work in progress

---
+++ easyhttp.js
@@ -0,0 +1,28 @@
+function easyHTTP(){
+    this.http = new XMLHttpRequest();
+}
+
+//Making HTTP GET Request
+easyHTTP.prototype.get = function(url, callback) {
+    this.http.open('GET', url, true);
+
+    let self = this;
+    this.http.onload = function() {
+        if (self.http.status === 200) {
+            callback(null, self.http.responseTex);
+        }else{
+            callback('Error: ' + self.http.status);
+        }
+    }
+
+
+    this.http.send();
+}
+
+//Making HTTP POST Request
+
+
+//Making HTTP PUT Request
+
+
+//Making HTTP DELETE Request
